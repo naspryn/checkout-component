f244df19bc209d0994406d9c48db3c018730c260
Naspryn <naspryn@gmail.com>
2017-11-18 18:28:47 +0100

    Javadoc

--- src/main/java/net/naspryn/checkoutservice/api/Checkout.java
+++ src/main/java/net/naspryn/checkoutservice/api/Checkout.java
@@ -6,5 +6,10 @@ import java.util.Map;
 
 public interface Checkout {
 
+    /**
+     * Methods returns total price for given products
+     * @param products Map contains products id - products count pairs
+     * @return total price for products in USD
+     */
     Money checkout(Map<Long, Integer> products);
 }
--- src/main/java/net/naspryn/checkoutservice/api/ProductsService.java
+++ src/main/java/net/naspryn/checkoutservice/api/ProductsService.java
@@ -4,7 +4,17 @@ import java.util.Set;
 
 public interface ProductsService {
 
+    /**
+     * Method returns product entity for given id
+     * @param id Product id
+     * @return Product entity with id, name, regular price and special price
+     */
     Product getProductById(Long id);
 
+    /**
+     * Method returns products entities by a set of ids
+     * @param ids Set of products ids
+     * @return Set of products entities with id, name, regular price and special price
+     */
     Set<Product> getProductsByIds(Set<Long> ids);
 }
